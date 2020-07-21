dc403b1a83eb0f56b512944e5255172c5e9b47c1
mamun ramdhan <mamunramdhan@gmail.com>
2020-07-21 11:19:04 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam quae odio sit adipisci harum inventore quisquam dignissimos nulla sint, expedita aliquam modi iste dolorem voluptatibus voluptates pariatur esse earum! Eos.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 
 </html>
